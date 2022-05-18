ba6ab0fc907c47c61b95a0122a8bbacb76be6c98
Julien Couignoux <j.couignoux@gmail.com>
2022-05-18 17:12:17 +0200

    add ticket and review admin

--- litreview/apps/admin.py
+++ litreview/apps/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from apps.models import Ticket, Review
 # Register your models here.
+
+
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    search_fields = ['title', 'description',
+                     'user', 'image', 'time_created']
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    search_fields = ['ticket', 'rating',
+                     'headline', 'body', 'user', 'time_created']
