4d2a9342a24dca52bd86ebad25d78d672b2a2f41
Julien Couignoux <j.couignoux@gmail.com>
2022-05-20 10:04:35 +0200

    add DATATIME INPUT

--- litreview/litreview/settings.py
+++ litreview/litreview/settings.py
@@ -106,14 +106,17 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Paris'
 
 USE_I18N = True
 
+USE_L10N = True
+
 USE_TZ = True
 
+DATETIME_INPUT_FORMAT = ["%Y-%m-%d %H:%M:%S"]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
